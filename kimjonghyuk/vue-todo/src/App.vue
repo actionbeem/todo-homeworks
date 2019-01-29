22aec061f1878d03917b5d9a023721ecc0fe3f1c
김종혁 <actionbeem929@gmail.com>
2019-01-30 00:04:36 +0900

    vue-todo 셋팅

---
+++ kimjonghyuk/vue-todo/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="appdasf">
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from './components/TodoHeader.vue';
+import TodoInput from './components/TodoInput.vue';
+import TodoList from './components/TodoList.vue';
+import TodoFooter from './components/TodoFooter.vue';
+
+export default {
+  components: {
+    'TodoHeader' : TodoHeader,
+    'TodoInput' : TodoInput,
+    'TodoList' : TodoList,
+    'TodoFooter' : TodoFooter,
+  }
+}
+</script>
+
+<style>
+body { text-align:center; background-color: #f5f5f5; padding:30px;}
+
+.input-todo { width:100%; font-size:16px; line-height:24px; box-shadow:5px 10px 10px rgba(0,0,0,0.1); border-style:groove; border:0; outline-width:0; padding:10px; }
+
+</style>
---
+++ kimjonghyuk/vue-todo/src/components/TodoInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <input class="input-todo" v-on:click="addTodo" type="text">
+</template>
+
+<script>
+export default {
+  methods : {
+    addTodo(){
+      console.log('aaa')
+    },
+  }
+}
+</script>
+
+<style>
+
+</style>
